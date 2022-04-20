8a40b32ac3da42eef73db76a189fad36f3031b50
Aydan <aydan2903@gmail.com>
2022-04-20 12:31:42 +0400

    ui table

--- src/companents/Table.jsx
+++ src/companents/Table.jsx
@@ -1,59 +1,59 @@
-import React, {useState, useEffect} from "react";
+import React, { useState, useEffect } from "react";
 import nextId from "react-id-generator";
 
 const Table = (datas) => {
   const items = datas.datas;
-  const [name, setName]=useState()
-  const[price,setPrice] =useState()
-  const [amount,setAmount] =useState()
-  const[date,setDate] =useState()
-  const[product,setProduct] =useState()
+  // const [name, setName]=useState()
+  // const[price,setPrice] =useState()
+  // const [amount,setAmount] =useState()
+  // const[date,setDate] =useState()
+  // const[product,setProduct] =useState()
 
+  // useEffect(() =>{
+  //   items.map((data) => {
+  //     setProduct(data.product)
+  //        setAmount(data.amount)
+  //        setDate(data.date)
+  //    })
+  // },[])
 
-  useEffect(() =>{
-    items.map((data) => {
-      setProduct(data.product)
-         setAmount(data.amount)
-         setDate(data.date)
-     })
-  },[amount,date])
-
-
-
-// products.map((product) =>{
-//     setName(product.name)
-//     setPrice(product.price)
-// })
+  // products.map((product) =>{
+  //     setName(product.name)
+  //     setPrice(product.price)
+  // })
   return (
     <table className="table-auto">
       <thead>
         <tr>
-          <th>Product Name</th>
-          <th>Order Price</th>
-          <th>Order Amount</th>
+          <th>Product Amount</th>
+          <th>Order Date</th>
+          <th>Order Name</th>
           <th>Total Price</th>
         </tr>
       </thead>
- <tbody>
-        
-            <tr>
-              <>
-                <td>{amount}</td>
-                <td>{date}</td>
+      <tbody>
+       
+          {items.map((data) => {
+            var products = data.product;
 
-                   {/*   {products.map((product) => {
-                  return (
-                    <>
-                      <td>{product.name}</td>
-                      <td>{product.price}</td>
-                    </>
-                  );
-                })}*/}
-              </>
-            </tr>
-         
-    
-      </tbody> 
+            return (
+              <tr>
+                <td>{data.amount}</td>
+                <td>{data.date}</td>
+                {products.map((product) => {
+              return (
+                <>
+                  <td>{product.name}</td>
+                  <td>{product.price}</td>
+                </>
+              );
+            })}
+              </tr>
+            );
+          })}
+      
+      
+      </tbody>
     </table>
   );
 };
